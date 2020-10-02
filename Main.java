322b6261d76b8a908ad03b00c2bca212eb766521
ramcj7742 <ramcj7742@wrdsb.ca>
2020-10-02 14:53:48 +0000

    Finished sum of user num

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * Calculates sum of 1 to user num
+ * @author Jaden Ramcharan
  */
 public class Main {
 
@@ -9,7 +10,24 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // Creates scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    //gets user number
+    System.out.println("Please enter an integer");
+    int num = input.nextInt();
+
+    //declare starting and accumulator variable
+    int START = 0;
+    int ACC = 0;
+
+    //accumulator
+    while(START < num){
+      START = START + 1;
+      ACC = ACC + START;
+    }
+    //Displays sum to user
+    System.out.println("The sum of 1 to " + num + " is " + ACC);
     
   }
 }
